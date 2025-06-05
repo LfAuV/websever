13075927bcb4162317d652c59547c1ce80452143
Luauv <2121271045@qq.com>
2025-06-05 21:42:37 +0800

    the second add

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
-#include <iostream>
+#include "unp.hpp"
 using namespace std;
 
-int main(){
-    while(true)
-    {
-        cout<<"Hello world"<<endl;
+/*
+
+argc：命令行参数数量
+argv：字符串数组，保存每个命令行参数
+
+
+argv[0]->程序本身的名字
+argv[1]->用户输入的参数
+
+*/
+int main(int argc, char**argv){
+
+    int sockfd, n;
+
+    char recvline[MAXLINE + 1];
+    /*
+    struct sockaddr_in {
+    sa_family_t    sin_family;   // 地址族（Address Family），AF_INET 表示 IPv4
+    in_port_t      sin_port;     // 端口号（使用 htons() 转为网络字节序）
+    struct in_addr sin_addr;     // IP地址结构体（使用 inet_pton 设置）
+    char           sin_zero[8];  // 填充字节（保持结构体大小与 sockaddr 一致）
+};*/
+    struct sockaddr_in servaddr;
+    
+    if(argc != 2){
+        err_quit("usage: a.out <IPaddress>");
     }
     return 0;
 }
---
+++ unp.hpp
@@ -0,0 +1,30 @@
+#ifndef __unp_h
+#define __unp_h
+
+#include<iostream>
+#include <iostream>
+#include <string>
+#include <cstring>      // memset
+#include <cstdlib>      // exit
+#include <unistd.h>     // read, close
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h> // sockaddr_in
+#include <arpa/inet.h>  // inet_pton
+#include <errno.h>
+
+#define MAXLINE 4096
+
+void err_quit(const std::string& msg){
+    std::cerr<< msg << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+void err_sys(const std::string& msg){
+    perror(msg.c_str());
+    exit(EXIT_FAILURE);
+}
+
+
+
+#endif
